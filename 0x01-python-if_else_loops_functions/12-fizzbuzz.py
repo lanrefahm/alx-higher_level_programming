3dac11f4205495e7e06fdefac443990b3353cc71
lanrefahm <lanrefahm@gmail.com>
2022-12-19 01:37:59 -0800

    12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Author - Fahm Mubarak
+"""Print the numbers from 11 to 1000 seperated by a space.
+for multiples of three, print Fizz instead of the number
+for multiples of five, print Buzz instead of the number.
+For multiples of three and five, print FizzBuzz instead of the number.
+"""
+
+
+def fizzbuzz():
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print ("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
